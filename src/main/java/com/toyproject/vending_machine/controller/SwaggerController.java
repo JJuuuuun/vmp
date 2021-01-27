666223bdbdda5ecbe99343da3603c52e3b721b26
jun <tpwns107@gmail.com>
2021-01-27 23:27:19 +0900

    Add Spring fox API documentation ui

---
+++ src/main/java/com/toyproject/vending_machine/controller/SwaggerController.java
@@ -0,0 +1,13 @@
+package com.toyproject.vending_machine.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SwaggerController {
+
+    @GetMapping
+    public String swagger() {
+        return "redirect:/swagger-ui/";
+    }
+}
